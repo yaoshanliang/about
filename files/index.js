cf6434072672b5e61c3990242f8e6fc6192a7ecb
iat <i@iat.net.cn>
2018-02-25 15:41:15 +0800

    update

---
+++ files/index.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?1892ade57a83d6416daa352555fbf340";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
