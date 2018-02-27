a1150679f96ee1b3d0781a217dd20fcb861e817b
iat <i@iat.net.cn>
2018-02-27 16:43:13 +0800

    update

--- projects.js
+++ projects.js
@@ -1,15 +1,46 @@
-var projects = [
-    {'id':1, 'type': 'web', 'name': '在线学习网站', 'tags': ['网站', '教育', 'Laravel'], 'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzDSANcohABXnVC90BCM039%20%281%29.png',
-        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
-        '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
-    'images': []},
+var projects = {
+    1: {
+        'type': '网站',
+        'name': '在线学习网站',
+        'tags': ['网站', '教育', 'Laravel'],
+        'period': '2017/02-2017/05',
+        'link': 'http://study.sshmt.com',
+        'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzDSANcohABXnVC90BCM039%20%281%29.png',
+        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
+        'images': [
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%89%8D%E5%8F%B0-%E9%A6%96%E9%A1%B5-%E4%B8%8A%E9%83%A8%E5%88%86.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%89%8D%E5%8F%B0-%E9%A6%96%E9%A1%B5-%E4%B8%8B%E9%83%A8%E5%88%86.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%89%8D%E5%8F%B0-%E8%AF%BE%E7%A8%8B-%E5%85%A8%E9%83%A8%E8%AF%BE%E7%A8%8B.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%89%8D%E5%8F%B0-%E8%80%83%E8%AF%95-%E5%85%A8%E9%83%A8%E8%80%83%E8%AF%95.png'
+        ]
+    },
 
-    {'id':1, 'type': 'web', 'name': '图片社交网站', 'tags': ['网站', '社交', 'Laravel'], 'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzo6AF7GQAArUJJA8V3M458%20%281%29.png',
+    2: {
+        'type': '网站',
+        'name': '图片社交网站',
+        'tags': ['网站', '社交', 'Laravel'],
+        'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzo6AF7GQAArUJJA8V3M458%20%281%29.png',
         'description': '【项目描述】社交网站前后台功能的开发、前台实现响应式，主要功能包含：用户注册、登录、上传/查看照片、分享、评论等。【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）； 系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）； 系统前后和后台的功能实现（基于PHP Laravel 框架）； 系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
-        'images': []},
+        'images': []
+    },
 
-    {'id':1, 'type': 'web', 'name': '金融曝光台', 'tags': ['网站', '金融'], 'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntz-uAS5CqAAFWcTbyIik450.png',
+    3: {
+        'type': '网站',
+        'name': '金融曝光台',
+        'tags': ['网站', '金融'],
+        'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntz-uAS5CqAAFWcTbyIik450.png',
         'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
         '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
-        'images': []},
-];
+        'images': []
+    },
+
+    5: {
+        'type': '微信公众号',
+        'name': '一路租车',
+        'tags': ['微信公众号', '旅游', 'Laravel'],
+        'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntz-uAS5CqAAFWcTbyIik450.png',
+        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
+        '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
+        'images': []
+    },
+};
