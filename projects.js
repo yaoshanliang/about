d65f9918f622e5fab3bb48699b7a279dcd634ff0
iat <i@iat.net.cn>
2018-02-27 11:07:10 +0800

    update

--- projects.js
+++ projects.js
@@ -1,4 +1,15 @@
 var projects = [
-    {'id':1, 'type': 'web', 'name': '网站', 'tags': ['laravel', 'php'], 'cover': ''},
-    {}
+    {'id':1, 'type': 'web', 'name': '在线学习网站', 'tags': ['网站', '教育', 'Laravel'], 'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzDSANcohABXnVC90BCM039%20%281%29.png',
+        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
+        '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
+    'images': []},
+
+    {'id':1, 'type': 'web', 'name': '图片社交网站', 'tags': ['网站', '社交', 'Laravel'], 'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzo6AF7GQAArUJJA8V3M458%20%281%29.png',
+        'description': '【项目描述】社交网站前后台功能的开发、前台实现响应式，主要功能包含：用户注册、登录、上传/查看照片、分享、评论等。【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）； 系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）； 系统前后和后台的功能实现（基于PHP Laravel 框架）； 系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
+        'images': []},
+
+    {'id':1, 'type': 'web', 'name': '金融曝光台', 'tags': ['网站', '金融'], 'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntz-uAS5CqAAFWcTbyIik450.png',
+        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
+        '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
+        'images': []},
 ];
