9cb112a451049c31ac67cb671d25414e9511763d
iat <i@iat.net.cn>
2018-02-28 10:33:14 +0800

    update

--- projects.js
+++ projects.js
@@ -2,8 +2,8 @@ var projects = {
     1: {
         'type': '网站',
         'name': '在线学习网站',
-        'tags': ['网站', '教育', 'Laravel'],
-        'period': '2017/02-2017/05',
+        'tags': ['网站', '教育', '视频', 'Laravel'],
+        'period': '2017/02-2017/03',
         'link': 'http://study.sshmt.com',
         'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzDSANcohABXnVC90BCM039%20%281%29.png',
         'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
@@ -18,29 +18,59 @@ var projects = {
     2: {
         'type': '网站',
         'name': '图片社交网站',
-        'tags': ['网站', '社交', 'Laravel'],
+        'tags': ['网站', '社交', 'Laravel', '微信接口开发'],
+        'period': '2017/05 — 2017/07',
+        'link': 'http://www.sightearth.com/web/welcome',
         'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntzo6AF7GQAArUJJA8V3M458%20%281%29.png',
-        'description': '【项目描述】社交网站前后台功能的开发、前台实现响应式，主要功能包含：用户注册、登录、上传/查看照片、分享、评论等。【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）； 系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）； 系统前后和后台的功能实现（基于PHP Laravel 框架）； 系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
-        'images': []
+        'description': '【项目描述】社交网站前后台功能的开发、前台实现响应式，主要功能包含：用户注册、登录、上传/查看照片、分享、评论等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）； 系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）； 系统前后和后台的功能实现（基于PHP Laravel 框架）； 系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
+        'images': [
+            'http://p4qunz9x0.bkt.clouddn.com/%E6%AC%A2%E8%BF%8E%E9%A1%B5.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E6%AC%A2%E8%BF%8E%E9%A1%B52.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E9%A6%96%E9%A1%B5.png'
+        ]
     },
 
     3: {
         'type': '网站',
         'name': '金融曝光台',
-        'tags': ['网站', '金融'],
+        'tags': ['网站', '金融', 'dagger框架', 'Echarts', '微博接口开发'],
+        'period': '2015/09 — 2015/12',
+        'link': 'http://money.finance.sina.com.cn/baoguangtai/view/',
         'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntz-uAS5CqAAFWcTbyIik450.png',
-        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
-        '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
-        'images': []
+        'description': '【项目描述】：金融曝光台由新浪财经开发、运营，是一个连接消费者和金融机构的通道。用户可在该平台填写投诉信息、查看投诉进度、对处理结果给予评价；金融机构可在该平台查看关于自家公司的投诉信息、反馈投诉处理进度。<br>【责任描述】：项目组成员，负责后台数据接口：企业入驻、投诉发表、投诉反馈；处理数据，供首页图表显示：未处理投诉排行、投诉地区排行、投诉分类排行等；测试部署。<br>【语言技术】：dagger框架、redis、Echarts、微博接口开发',
+        'images': [
+            'http://p4qunz9x0.bkt.clouddn.com/%E9%87%91%E8%9E%8D%E6%9B%9D%E5%85%89%E5%8F%B0-%E9%A6%96%E9%A1%B5.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E9%87%91%E8%9E%8D%E6%9B%9D%E5%85%89%E5%8F%B0-%E9%A6%96%E9%A1%B52.jpg',
+            'http://p4qunz9x0.bkt.clouddn.com/%E9%87%91%E8%9E%8D%E6%9B%9D%E5%85%89%E5%8F%B0-%E9%A6%96%E9%A1%B53.jpg',
+            'http://p4qunz9x0.bkt.clouddn.com/%E9%87%91%E8%9E%8D%E6%9B%9D%E5%85%89%E5%8F%B0-%E4%BC%81%E4%B8%9A%E5%85%A5%E9%A9%BB.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E9%87%91%E8%9E%8D%E6%9B%9D%E5%85%89%E5%8F%B0-%E6%8A%95%E8%AF%89%E5%A4%84%E7%90%86.png'
+        ]
     },
 
     5: {
         'type': '微信公众号',
         'name': '一路租车',
-        'tags': ['微信公众号', '旅游', 'Laravel'],
-        'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpnS1ntz-uAS5CqAAFWcTbyIik450.png',
-        'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。' +
-        '【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
-        'images': []
+        'tags': ['微信公众号', '旅游', '地图', 'Laravel', '微信接口开发'],
+        'period': '2017/10 — 2017/11',
+        'link': '微信公众号: 一路出行服务号',
+        'cover': 'http://p4qunz9x0.bkt.clouddn.com/CgpvS1pMctWAAZZHAAR89QaX7fc512.png',
+        'description': '【项目描述】一路租车是一款互联网巴士出行平台，平台包含：包车下单、支付、议价、评论、拼团、路线定制等功能。',
+        'images': [
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%8C%85%E8%BD%A6-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%8C%85%E8%BD%A6-%E5%87%BA%E5%8F%91%E5%9C%B0%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%8C%85%E8%BD%A6-%E7%9F%AD%E4%BF%A1%E9%AA%8C%E8%AF%81%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E5%8C%85%E8%BD%A6-%E9%A2%84%E7%BA%A6%E6%88%90%E5%8A%9F%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E8%AE%A2%E5%8D%95-%E5%BE%85%E6%8A%A5%E4%BB%B7%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E8%AE%A2%E5%8D%95-%E5%B7%B2%E6%8A%A5%E4%BB%B7%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E8%AE%A2%E5%8D%95-%E5%BE%85%E5%87%BA%E8%A1%8C%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E8%AE%A2%E5%8D%95-%E5%B7%B2%E5%87%BA%E8%A1%8C%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E6%8B%BC%E5%9B%A2-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E6%8B%BC%E5%9B%A2-%E6%94%AF%E4%BB%98%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E8%B7%AF%E7%BA%BF-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E8%B7%AF%E7%BA%BF-%E5%AE%9A%E5%88%B6%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E6%90%9C%E7%B4%A2%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+            'http://p4qunz9x0.bkt.clouddn.com/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E6%8A%95%E8%AF%89%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+        ]
     },
 };
