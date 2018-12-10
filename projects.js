3fba4f14bdb36a93b1de9f0f80e98eb2b300fe27
iat <i@iat.net.cn>
2018-12-10 15:47:27 +0800

    update

--- projects.js
+++ projects.js
@@ -194,7 +194,7 @@ var projects = {
         'name': '在线学习网站',
         'tags': ['网站', '教育', '视频', 'Laravel'],
         'period': '2017/02-2017/03',
-        'link': 'http://study.sshmt.com',
+        'link': 'http://study.iat.net.cn',
         'cover': '%E5%89%8D%E5%8F%B0-%E9%A6%96%E9%A1%B5-%E4%B8%8A%E9%83%A8%E5%88%86-cover.png',
         'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
         'images': [
@@ -216,7 +216,7 @@ var projects = {
         'name': '图片社交网站',
         'tags': ['网站', '社交', 'Laravel', '微信接口开发'],
         'period': '2017/05 — 2017/07',
-        'link': 'http://www.sightearth.com/web/welcome',
+        'link': 'http:/sight.iat.net.cn',
         'cover': '%E6%AC%A2%E8%BF%8E%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
         'description': '【项目描述】社交网站前后台功能的开发、前台实现响应式，主要功能包含：用户注册、登录、上传/查看照片、分享、评论等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）； 系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）； 系统前后和后台的功能实现（基于PHP Laravel 框架）； 系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
         'images': [
@@ -225,7 +225,6 @@ var projects = {
             '%E9%A6%96%E9%A1%B5.png'
         ]
     },
-
 
     'car': {
         'type': '微信公众号',
