ba56a34988bf2b579333e2d47c4c9b470c34bb2d
iat <i@iat.net.cn>
2019-04-26 16:30:28 +0800

    update

--- projects.js
+++ projects.js
@@ -1,16 +1,29 @@
-var SITE = 'http://image.iat.net.cn/';
 
 var projects = {
+    'yoosense-app': {
+        'type': 'APP',
+        'name': '基督徒婚恋专属平台-APP',
+        'tags': ['APP'],
+        'period': '2018/10 — 2019/02',
+        'link': 'http://www.yoosense.com/',
+        'cover': '【友信APP】cover.png',
+        'description': '【项目描述】：社交APP',
+        'images': [
+            { name: 'yoosense-app-1.png', width: 360, height: 'auto'},
+            { name: 'yoosense-app-2.png', width: 360, height: 'auto'},
+            { name: 'yoosense-app-3.png', width: 360, height: 'auto'},
+        ]
+    },
     'szhlw-aliapp': {
         'type': '支付宝小程序',
         'name': '垃圾分类小程序',
         'tags': ['支付宝小程序'],
         'period': '2018/09 — 2018/10',
         'link': '扫码访问',
-        'cover': 'szhlw-aliapp-qrcode.png',
+        'cover': '【互联网+支付宝小程序】qrcode.jpg',
         'description': '【项目描述】：苏州市互联网+垃圾分类综合管理系统项目借鉴国内外、各行业相关建设的先进经验，特别是上海“绿色账户”工作，以可回收物的回收为突破口，通过“互联网+”的模式，建立垃圾分类综合管理服务平台，改变原有的可回收物回收模式，同时系统应具有很强的扩展性，可以兼顾大件垃圾和有害垃圾回收管理，未来还将扩展对有害垃圾的回收管理，鼓励更多的居民，特别是年轻人参与垃圾分类，建设高效、持续的垃圾分类回收新生态。',
         'images': [
-            'szhlw-aliapp-qrcode.png',
+            '【互联网+支付宝小程序】qrcode.jpg',
             '<br/>',
             'szhlw-1.png',
             'szhlw-2.png',
@@ -32,7 +45,7 @@ var projects = {
         'tags': ['微信公众号'],
         'period': '2018/08 — 2018/09',
         'link': '微信公众号：易丢丢垃圾回收',
-        'cover': 'szhlw-logo.png',
+        'cover': '【互联网+微信公众号】qrcode.jpg',
         'description': '【项目描述】：苏州市互联网+垃圾分类综合管理系统项目借鉴国内外、各行业相关建设的先进经验，特别是上海“绿色账户”工作，以可回收物的回收为突破口，通过“互联网+”的模式，建立垃圾分类综合管理服务平台，改变原有的可回收物回收模式，同时系统应具有很强的扩展性，可以兼顾大件垃圾和有害垃圾回收管理，未来还将扩展对有害垃圾的回收管理，鼓励更多的居民，特别是年轻人参与垃圾分类，建设高效、持续的垃圾分类回收新生态。',
         'images': [
             '<br/>',
@@ -56,7 +69,7 @@ var projects = {
         'tags': ['微信小程序', 'ibeacon'],
         'period': '2018/06 — 2018/07',
         'link': '扫码访问',
-        'cover': 'weapp-ibeacon-qrcode.jpg',
+        'cover': '【ibeacon】qrcode.jpg',
         'description': '【项目描述】：根据ibeacon与手机之间的距离，展示不同场景的营销页面，包含图文、视频、音频、红包、优惠券等，适用于商场里的营销推广。',
         'images': [
             'weapp-ibeacon-qrcode.jpg',
@@ -82,7 +95,7 @@ var projects = {
         'tags': ['网站', 'wordpress'],
         'period': '2018/04 — 2018/05',
         'link': 'http://149.28.69.69/',
-        'cover': 'sanlac-cover.png',
+        'cover': '【sanlac】cover.png',
         'description': '【项目描述】：wordpress主题开发',
         'images': [
             'sanlac-1.png',
@@ -96,7 +109,7 @@ var projects = {
         'tags': ['微信公众号', 'Laravel'],
         'period': '2018/03 — 2018/03',
         'link': '微信公众号: 苏州博仁跆拳道',
-        'cover': 'boren-qrcode.jpg',
+        'cover': '【博仁】qrcode.jpg',
         'description': '【项目描述】：公众号内报名比赛活动并缴费',
         'images': [
             'boren-qrcode.jpg',
@@ -114,7 +127,7 @@ var projects = {
         'tags': ['网站', '响应式'],
         'period': '2018/02 — 2018/02',
         'link': 'http://www.yoosense.com/',
-        'cover': 'yoosense-cover.png',
+        'cover': '【友信网站】cover.png',
         'description': '【项目描述】：社交APP的宣传介绍页面',
         'images': [
             'yoosense-1.png'
@@ -127,7 +140,7 @@ var projects = {
         'tags': ['手机H5', '视频', '微信接口开发'],
         'period': '2018/01 — 2018/01',
         'link': '扫码访问',
-        'cover': '%E5%85%B3%E7%88%B1%E5%8D%95%E8%BA%AB%E7%8B%97-cover.png',
+        'cover': '【关爱单身狗】cover.png',
         'description': '【项目描述】：<br>1、手机H5网页，强制横屏;<br>2、首先进入Loading页，此过程用来从后台下载视频;<br>3、Loading完成后，进入视频播放页，强制全屏播放；<br>4、视频播放到指定时间，自动暂停，提示用户分享给好友或者朋友圈；<br>5、分享完成后，继续播放<br>6、视频播放完成后，请用户提交一个信息',
         'images': [
             '%E5%85%B3%E7%88%B1%E5%8D%95%E8%BA%AB%E7%8B%97-cover.png',
@@ -144,7 +157,7 @@ var projects = {
         'tags': ['网站', 'wordpress', '响应式'],
         'period': '2017/12 — 2017/12',
         'link': 'https://www.edinnovaedu.com/',
-        'cover': '%E5%B0%94%E6%96%B0%E6%95%99%E8%82%B2-cover.jpg',
+        'cover': '【尔新教育】cover.jpg',
         'description': '【项目描述】：教育机构官网',
         'images': [
             '%E5%B0%94%E6%96%B0%E6%95%99%E8%82%B2-1.jpg',
@@ -159,7 +172,7 @@ var projects = {
         'tags': ['微信'],
         'period': '2017/08 — 2017/9',
         'link': 'https://github.com/yaoshanliang/vbot-tuling',
-        'cover': 'vbot-tuling-logo.png',
+        'cover': '【图灵机器人】cover.png',
         'description': '【项目描述】：调用图灵机器人接口，自动回复微信消息。',
         'images': [
             '<br/>',
@@ -175,7 +188,7 @@ var projects = {
         'tags': ['管理信息系统', '政府', 'Laravel'],
         'period': '2017/11 — 2018/01',
         'link': '政府内网',
-        'cover': '%E8%B5%84%E4%BA%A7%E7%AE%A1%E7%90%86%E7%B3%BB%E7%BB%9F-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+        'cover': '【资产管理系统】cover.png',
         'description': '【项目描述】：资产管理系统实现用户在线管理，物资信息的录入、修改、删除，同时实现对物资的采购、入库、出库、签收等操作的电子化流程管理，支持查询物资的状态及操作明细，方便管理员估算资产及做好预算等。',
         'images': [
             '%E8%B5%84%E4%BA%A7%E7%AE%A1%E7%90%86%E7%B3%BB%E7%BB%9F-%E9%A6%96%E9%A1%B5.png',
@@ -195,7 +208,7 @@ var projects = {
         'tags': ['网站', '教育', '视频', 'Laravel'],
         'period': '2017/02-2017/03',
         'link': 'http://study.iat.net.cn',
-        'cover': '%E5%89%8D%E5%8F%B0-%E9%A6%96%E9%A1%B5-%E4%B8%8A%E9%83%A8%E5%88%86-cover.png',
+        'cover': '【在线学习网站】cover.png',
         'description': '【项目描述】公务员在线视频学习及考试网站的开发。用户观看课程视频进行学习，观看后进行在线答题，系统需记录观看进度，进行成绩、学时、学分等统计。后台进行用户管理、课程管理、试题管理等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）；系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）；系统前后和后台的功能实现（基于PHP Laravel 框架）；系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
         'images': [
             '%E5%89%8D%E5%8F%B0-%E9%A6%96%E9%A1%B5-%E4%B8%8A%E9%83%A8%E5%88%86.png',
@@ -217,7 +230,7 @@ var projects = {
         'tags': ['网站', '社交', 'Laravel', '微信接口开发'],
         'period': '2017/05 — 2017/07',
         'link': 'http:/sight.iat.net.cn',
-        'cover': '%E6%AC%A2%E8%BF%8E%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
+        'cover': '【sight】cover.png',
         'description': '【项目描述】社交网站前后台功能的开发、前台实现响应式，主要功能包含：用户注册、登录、上传/查看照片、分享、评论等。<br>【项目职责】负责业务功能（PC端B/S系统）的开发，具体包含： 系统的架构设计及数据库设计（MySQL）； 系统前台和后台的页面制作（Bootstrap+JQuery+Html+CSS）； 系统前后和后台的功能实现（基于PHP Laravel 框架）； 系统的部署； 配合测试团队对系统局部或整体功能的测试，并根据反馈进行问题修复和调优。',
         'images': [
             '%E6%AC%A2%E8%BF%8E%E9%A1%B5.png',
@@ -232,7 +245,7 @@ var projects = {
         'tags': ['微信公众号', '旅游', '地图', 'Laravel', '微信接口开发'],
         'period': '2017/10 — 2017/11',
         'link': '微信公众号: 一路出行服务号',
-        'cover': '%E5%8C%85%E8%BD%A6-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC%203.png',
+        'cover': '【一路租车】cover.png',
         'description': '【项目描述】一路租车是一款互联网巴士出行平台，平台包含：包车下单、支付、议价、评论、拼团、路线定制等功能。',
         'images': [
             '%E5%8C%85%E8%BD%A6-%E9%A6%96%E9%A1%B5%E7%9A%84%E5%89%AF%E6%9C%AC.png',
@@ -259,7 +272,7 @@ var projects = {
         'tags': ['网站', 'Laravel', '开源'],
         'period': '2017/03 — 至今',
         'link': 'https://github.com/yaoshanliang/laravel',
-        'cover': 'laravel-cover.png',
+        'cover': '【laravel】cover.png',
         'description': '【项目描述】基于Laravel的上手即用框架,内置前台、后台、api、微信公众号、微信小程序功能模块。<br>【开源地址】: <a href="https://github.com/yaoshanliang/laravel" target="_blank">https://github.com/yaoshanliang/laravel</a> ',
         'images': [
         ]
@@ -271,7 +284,7 @@ var projects = {
         'tags': ['网站', '金融', 'dagger框架', 'Echarts', '微博接口开发'],
         'period': '2015/09 — 2015/12',
         'link': 'http://money.finance.sina.com.cn/baoguangtai/view/',
-        'cover': 'CgpnS1ntz-uAS5CqAAFWcTbyIik450%20%281%29.png',
+        'cover': '【金融曝光台】cover.png',
         'description': '【项目描述】：金融曝光台由新浪财经开发、运营，是一个连接消费者和金融机构的通道。用户可在该平台填写投诉信息、查看投诉进度、对处理结果给予评价；金融机构可在该平台查看关于自家公司的投诉信息、反馈投诉处理进度。<br>【责任描述】：项目组成员，负责后台数据接口：企业入驻、投诉发表、投诉反馈；处理数据，供首页图表显示：未处理投诉排行、投诉地区排行、投诉分类排行等；测试部署。<br>【语言技术】：dagger框架、redis、Echarts、微博接口开发',
         'images': [
             '%E9%87%91%E8%9E%8D%E6%9B%9D%E5%85%89%E5%8F%B0-%E9%A6%96%E9%A1%B5.png',
@@ -288,7 +301,7 @@ var projects = {
         'tags': ['网站', 'wordpress', 'ThinkPHP'],
         'period': '2017/03 — 2017/03',
         'link': 'http://sues.net.cn/Sds/Home/Index',
-        'cover': '%E8%8B%8F%E5%B7%9E%E5%A4%A7%E5%AD%A6%E5%AE%9E%E9%AA%8C%E5%AD%A6%E6%A0%A1%E7%BD%91%E7%AB%99-cover.png',
+        'cover': '【苏州大学实验学校网站】cover.png',
         'description': '【项目描述】：学校官网, 包含学校概况、新闻中心、党政之窗、教学科研、国际交流、关工在线、招生招聘等。',
         'images': [
             '%E8%8B%8F%E5%B7%9E%E5%A4%A7%E5%AD%A6%E5%AE%9E%E9%AA%8C%E5%AD%A6%E6%A0%A1%E7%BD%91%E7%AB%99-%E9%A6%96%E9%A1%B5.png',
@@ -303,7 +316,7 @@ var projects = {
         'tags': ['微信小程序', 'wordpress'],
         'period': '2017/01 — 2017/01',
         'link': 'https://github.com/yaoshanliang/weapp-ssha',
-        'cover': '%E7%9B%9B%E4%B8%96%E5%8D%8E%E5%AE%89%E5%B0%8F%E7%A8%8B%E5%BA%8F%E7%A0%81.jpeg',
+        'cover': '【盛世华安小程序】qrcode.jpeg',
         'description': '【项目描述】：提供公司新闻资讯、产品服务、宣传视频、联系方式等。',
         'images': [
             '%E7%9B%9B%E4%B8%96%E5%8D%8E%E5%AE%89%E5%B0%8F%E7%A8%8B%E5%BA%8F%E7%A0%81.jpeg',
@@ -318,7 +331,7 @@ var projects = {
         'tags': ['微信小程序', 'wordpress'],
         'period': '2018/01 — 2018/01',
         'link': '扫码访问',
-        'cover': '%E5%98%89%E7%A6%84%E8%AE%AF%E6%B1%87%E5%B0%8F%E7%A8%8B%E5%BA%8F-qrcode.jpeg',
+        'cover': '【嘉禄讯汇小程序】qrcode.jpeg',
         'description': '【项目描述】：提供公司新闻资讯、产品服务、宣传视频、联系方式等。',
         'images': [
             '%E5%98%89%E7%A6%84%E8%AE%AF%E6%B1%87%E5%B0%8F%E7%A8%8B%E5%BA%8F-qrcode.jpeg',
@@ -333,7 +346,7 @@ var projects = {
         'tags': ['网站', '触摸屏', 'Laravel'],
         'period': '2017/03 — 至今',
         'link': '登录访问',
-        'cover': '%E4%B8%80%E4%BD%93%E6%9C%BA%E9%A6%96%E9%A1%B5-cover.png',
+        'cover': '【一体机】cover.png',
         'description': '【项目描述】：一体机触摸屏项目，在惠民通项目用于在服务站展示政务服务等信息, 包含志愿者服务、预约挂号、血压血糖测量、寄快递、社区治理、智慧养老等。',
         'images': [
             '%E4%B8%80%E4%BD%93%E6%9C%BA%E9%A6%96%E9%A1%B5.png',
@@ -348,7 +361,7 @@ var projects = {
         'tags': ['手机H5', 'charts'],
         'period': '2016/11 — 2016/11',
         'link': '扫码访问',
-        'cover': '%E8%A1%80%E5%8E%8B%E8%A1%80%E7%B3%96%E6%A3%80%E6%B5%8B-%E6%89%AB%E7%A0%81%E8%AE%BF%E9%97%AE.png',
+        'cover': '【血压血糖检测】qrcode.png',
         'description': '【项目描述】：基于血压血糖仪器测量汇报的数据，通过图表的形式将血压血糖的数据展示出来；用户可以绑定自己家人的账号，方便对家人数据的查看；对异常的血压血糖用突出的颜色标注，后台定期发送消息提醒。',
         'images': [
             '%E8%A1%80%E5%8E%8B%E8%A1%80%E7%B3%96%E6%A3%80%E6%B5%8B-%E6%89%AB%E7%A0%81%E8%AE%BF%E9%97%AE.png',
@@ -368,7 +381,7 @@ var projects = {
         'tags': ['网站', 'wordpress'],
         'period': '2016/10 — 2016/10',
         'link': 'http://www.jsssha.com/',
-        'cover': 'ssha-cover.png',
+        'cover': '【盛世华安网站】cover.png',
         'description': '【项目描述】：提供公司新闻资讯、产品服务、宣传视频、联系方式等。',
         'images': [
             'ssha-1.png',
@@ -383,7 +396,7 @@ var projects = {
         'tags': ['网站', 'laravel', 'redis'],
         'period': '2015/12 — 2016/03',
         'link': 'https://github.com/yaoshanliang/ucenter',
-        'cover': '%E7%94%A8%E6%88%B7%E4%B8%AD%E5%BF%83-cover.png',
+        'cover': '【用户中心】cover.png',
         'description': '【项目描述】：集“统一身份认证服务”、“用户系统”、“权限系统”、“日志系统”、“文件系统”、“邮件服务”、“短信服务”等于一体， 旨在为开发者提供统一的管理、调用服务，让开发更简单！',
         'images': [
             '%E7%94%A8%E6%88%B7%E4%B8%AD%E5%BF%83-%E7%99%BB%E5%BD%95.png',
@@ -404,7 +417,7 @@ var projects = {
         'tags': ['手机H5', '微信接口开发', 'memcached'],
         'period': '2015/08 — 2015/09',
         'link': '扫码访问',
-        'cover': '%E8%BE%BE%E6%B2%83%E6%96%AF%E5%8F%82%E4%BC%9A%E7%A5%9E%E5%99%A8-qrcode.png',
+        'cover': '【达沃斯参会神器】qrcode.png',
         'description': '【项目描述】：服务于2015年达沃斯领军者年会的移动端信息页面，结合微信展示、分享的功能，达到快速查阅会议信息，方便了解会议咨询的效果。',
         'images': [
             '%E8%BE%BE%E6%B2%83%E6%96%AF%E5%8F%82%E4%BC%9A%E7%A5%9E%E5%99%A8-qrcode.png',
@@ -424,7 +437,7 @@ var projects = {
         'tags': ['管理信息系统'],
         'period': '2015/08 — 2015/09',
         'link': 'http://sklcc.suda.edu.cn/dsz',
-        'cover': '%E5%AF%BC%E5%B8%88%E5%88%B6-cover1.png',
+        'cover': '【导师制】cover.png',
         'description': '【项目描述】：老师在线给学生安排任务，学生向老师提出问题，构建在线学习交流的平台，并作为信息维护、项目管理、成果统计使用。',
         'images': [
             '%E5%AF%BC%E5%B8%88%E5%88%B6-%E5%9B%BE%E7%89%871.png',
@@ -441,7 +454,7 @@ var projects = {
         'tags': ['网站', 'wordpress'],
         'period': '2015/05 — 2015/09',
         'link': 'http://www.ylfj.com.cn/',
-        'cover': '%E5%8F%8B%E8%81%94%E7%BD%91%E7%AB%99-cover1.png',
+        'cover': '【友联网站】cover.png',
         'description': '【项目描述】：苏州友联纺工装备科技股份公司需要搭建公司网站，用于宣传介绍公司的信息、新闻、产品等，提供“配置方案”和“价格计算”的功能，供访客自行配置商品及计算价格。公司后台需要维护各种信息、收集访客日志的功能，从而实现定向服务推广。结合移动端微信技术，达到快速分享、推广宣传的效果。',
         'images': [
             '%E5%8F%8B%E8%81%94%E7%BD%91%E7%AB%99-1.png',
@@ -457,7 +470,7 @@ var projects = {
         'tags': ['管理信息系统', 'EaglePHP'],
         'period': '2013/12 — 2014/03',
         'link': '内网访问',
-        'cover': '%E5%AD%A6%E7%94%9F%E7%A7%91%E7%A0%94%E9%A1%B9%E7%9B%AE%E7%AE%A1%E7%90%86%E5%B9%B3%E5%8F%B0-cover.jpg',
+        'cover': '【学生科研项目管理平台】cover.jpg',
         'description': '【项目描述】：“科研项目管理平台”是为方便管理科研项目而开发的平台,在同一平台的基础上,项目主持人、辅导员、学院管理员一级学校管理员可以在统一的平台上进行查看、审核、下载等管理操作。',
         'images': [
             '%E5%AD%A6%E7%94%9F%E7%A7%91%E7%A0%94%E9%A1%B9%E7%9B%AE%E7%AE%A1%E7%90%86%E5%B9%B3%E5%8F%B0-1.jpg',
